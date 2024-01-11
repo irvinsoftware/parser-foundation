7e2f60630dc93bb711870ed59eaf9ee261222914
Nate Irvin <nate.irvin@gmail.com>
2024-01-11 12:54:42 -0700

    adding more token access methods

--- Parser/Token.cs
+++ Parser/Token.cs
@@ -1,12 +1,31 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Irvin.Extensions.Collections;
 
 namespace Irvin.Parser
 {
     public class Token
     {
-        public string Content { get; internal set; }
+        private string _content;
+
+        public string Content
+        {
+            get
+            {
+                return _content;
+            }
+            internal set
+            {
+                _content = value;
+                ContentUpper = value?.ToUpper();
+                ContentLower = value?.ToLower();
+            }
+        }
+
+        public string ContentUpper { get; private set; }
+        public string ContentLower { get; private set; }
+        
         public string SubContent { get; set; }
         public int StartPosition { get; internal set; }
         public bool IsDelimiter { get; internal set; }
@@ -18,20 +37,17 @@ namespace Irvin.Parser
 
         public static string Join(IEnumerable<Token> tokens)
         {
-            return String.Join("", tokens.Select(x => x.Content).ToArray());
+            return tokens.Select(x => x.Content).Concatenate();
         }
 
-        public bool IsSpace
-        {
-            get { return Content == " " || Content == "\t"; }
-        }
+        public bool IsSpace => Content == " " || Content == "\t";
+        public bool IsSpaceOrNewLine => IsSpace || Content == Environment.NewLine;
+        public bool IsSpaceOrLineFeed => IsSpace || Content == "\n";
+        
+        /// <summary>
+        /// The token's text matches the Unicode definition of whitespace.
+        /// </summary>
+        public bool IsWhitespace => Content.All(char.IsWhiteSpace);
 
-        public bool IsSpaceOrNewLine
-        {
-            get
-            {
-                return IsSpace || Content == Environment.NewLine;
-            }
-        }
     }
 }
